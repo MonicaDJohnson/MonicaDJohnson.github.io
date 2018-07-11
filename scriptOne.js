43aae4ed27be20fcf83bcd5ca2efe76221e891a6
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-07-11 14:31:11 -0400

    live portfolio

---
+++ scriptOne.js
@@ -0,0 +1,5 @@
+﻿//function ChangeP() {
+//    document.getElementsByTagName("p")/*[0]*/.style.fontSize = "11px"; //I couldn't see any other use for Javascript on my page other than this function from our example. I can't get it to work so it is commented out.
+//    //document.getElementsByTagName("p")[1].style.fontSize = "11px";
+//    //document.getElementsByTagName("p")[2].style.fontSize = "11px";
+//}
---
+++ scriptThree.js
@@ -0,0 +1,5 @@
+﻿//function ChangeP() {
+//    document.getElementsByTagName("p")/*[0]*/.style.fontSize = "11px"; //see comment on script one.
+//    //document.getElementsByTagName("p")[1].style.fontSize = "11px";
+//    //document.getElementsByTagName("p")[2].style.fontSize = "11px";
+//}
